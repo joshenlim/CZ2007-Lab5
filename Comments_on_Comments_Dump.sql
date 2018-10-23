ab010a30cadb80776493539954f54ca9dd7c0e61
Kenneth Ng <kennethng95@gmail.com>
2018-10-23 23:25:03 +0800

    Updated comments, ConC, ConP, Shops, Pdts and Users

--- Comments_on_Comments_Dump.sql
+++ Comments_on_Comments_Dump.sql
@@ -2,3 +2,5 @@
 
 INSERT INTO Comments_on_Comments VALUES(6, 2);
 INSERT INTO Comments_on_Comments VALUES(7, 4);
+INSERT INTO Comments_on_Comments VALUES(8, 10);
+INSERT INTO Comments_on_Comments VALUES(13, 15);
