cf5a7aee2e8e9357570bdf98da83559de2fb4db8
Ng Jing Rui <ngjingrui@hotmail.com>
2018-10-23 22:41:31 +0800

    Added more dummy inputs for Review_Dump & rectified errors in Order_items

--- Review_Dump.sql
+++ Review_Dump.sql
@@ -4,4 +4,19 @@ INSERT INTO Review VALUES(1, 1, 'iPhone XS', 'Nubox', 5, 1, '2018-10-02 15:27:40
 INSERT INTO Review VALUES(1, 1, 'KAVAL Hoodie', 'Royal Sporting House', 1, 2, '2018-10-05 13:35:20.500');
 INSERT INTO Review VALUES(2, 1, 'Ultraboost Shoes', 'Adidas', 4, 3, '2018-10-08 17:21:30.791');
 INSERT INTO Review VALUES(3, 2, 'Pureboost RBL Shoes', 'Adidas', 3, 5, '2018-10-15 15:01:02.123');
-INSERT INTO Review VALUES(5, 10, 'Galaxy S6', 'Nubox', 4, 5, '2018-10-22 10:15:03.153');
+INSERT INTO Review VALUES(5, 4, 'Galaxy S6', 'Nubox', 4, 5, '2018-06-22 10:15:03.153'); --I changed user_id
+INSERT INTO Review VALUES(6, 5, 'iPhone XS', 'iStudio', 5, _, '2018-10-17 02:03:01.429');
+INSERT INTO Review VALUES(7, 6, 'Galaxy S7', 'Nubox', 4, _, '2018-06-24 10:12:34.129');
+INSERT INTO Review VALUES(7, 6, 'Galaxy S6', 'Nubox', 4, _, '2018-06-24 11:12:34.189');
+INSERT INTO Review VALUES(8, 7, 'iPhone XS', 'Nubox', 5, _, '2018-06-25 13:29:52.120');
+INSERT INTO Review VALUES(9, 8, 'Galaxy S6', 'iStudio', 4, _, '2018-06-16 10:14:21.303');
+INSERT INTO Review VALUES(10, 9, 'Galaxy S9', 'iStudio', 3, _, '2018-06-10 10:59:19.603');
+INSERT INTO Review VALUES(11, 10, 'iPhone XR', 'iStudio', 5, _, '2018-10-24 06:39:19.623');
+INSERT INTO Review VALUES(12, 11, 'iPhone X', 'iStudio', 5, _, '2018-06-12 10:35:34.417'); --RETURNED
+INSERT INTO Review VALUES(13, 12, 'iPhone XS', 'NuBox', 2, _, '2018-06-20 13:51:23.329');
+INSERT INTO Review VALUES(14, 13, 'iPhone X', 'iStudio', 3, _, '2018-06-10 14:23:01.257');
+INSERT INTO Review VALUES(15, 14, 'Ultraboost Shoes','Royal Sporting House', 5, _, '2018-08-20 13:51:49.285');
+INSERT INTO Review VALUES(16, 15, 'iPhone X', 'Nubox', 4, _, '2018-08-20 13:16:01.524');
+INSERT INTO Review VALUES(17, 16, 'KAVAL Cap', 'Royal Sporting House', 3, _, '2018-09-01 13:52:46.759');
+INSERT INTO Review VALUES(18, 17, 'Ultraboost Shoes', 'Adidas', 5, _, '2018-08-26 10:24:35.273');
+INSERT INTO Review VALUES(19, 18, 'Pureboost RBL Shoes', 'Adidas', 4, _, '2018-08-20 13:21:35.512');
