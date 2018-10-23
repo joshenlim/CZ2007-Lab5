d79407d3c2bf19bbc04ba3b723b8f69ad1391a73
unknown <bryanyeap5@gmail.com>
2018-10-23 16:56:57 +0800

    updated complains and employees with new table values

--- Complaints_on_Shops_Dump.sql
+++ Complaints_on_Shops_Dump.sql
@@ -3,3 +3,6 @@
 INSERT INTO Complaints_on_Shops VALUES(5, 'Royal Sporting House');
 INSERT INTO Complaints_on_Shops VALUES(6, 'iStudio');
 INSERT INTO Complaints_on_Shops VALUES(7, 'Nubox');
+INSERT INTO Complaints_on_Shops VALUES(9, 'Addidas');
+INSERT INTO Complaints_on_Shops VALUES(10, 'Newstead Technologies');
+INSERT INTO Complaints_on_Shops VALUES(13, 'Xiaomi');
