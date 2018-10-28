eb573d80661d57f1cf393502b1f2d58a9122f1aa
Ng Jing Rui <ngjingrui@hotmail.com>
2018-10-28 22:19:15 +0800

    Updated price_history

--- Price_History_Dump.sql
+++ Price_History_Dump.sql
@@ -54,27 +54,6 @@ INSERT INTO Price_History VALUES('iStudio', 'iPhone XR', 1859, '2018-06-01', '20
 INSERT INTO Price_History VALUES('iStudio', 'iPhone XR', 1799, '2018-08-01', '2018-08-31');
 INSERT INTO Price_History VALUES('iStudio', 'iPhone XR', 1899, '2018-08-31', '2018-11-26');
 
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1359, '2018-01-01', '2018-03-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1344, '2018-03-01', '2018-05-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1200, '2018-05-01', '2018-06-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1240, '2018-06-01', '2018-08-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1222, '2018-08-01', '2018-08-31');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S9', 1110, '2018-08-31', '2018-11-26');
-
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1350, '2018-01-01', '2018-03-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1294, '2018-03-01', '2018-05-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1199, '2018-05-01', '2018-06-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1040, '2018-06-01', '2018-08-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1180, '2018-08-01', '2018-08-31');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S7', 1050, '2018-08-31', '2018-11-26');
-
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 1130, '2018-01-01', '2018-03-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 1100, '2018-03-01', '2018-05-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 1199, '2018-05-01', '2018-06-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 1001, '2018-06-01', '2018-08-01');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 1080, '2018-08-01', '2018-08-31');
-INSERT INTO Price_History VALUES('iStudio', 'Galaxy S6', 999, '2018-08-31', '2018-11-26');
-
 INSERT INTO Price_History VALUES('Royal Sporting House', 'Ultraboost Shoes', 265, '2018-01-01', '2018-03-01');
 INSERT INTO Price_History VALUES('Royal Sporting House', 'Ultraboost Shoes', 255, '2018-03-01', '2018-05-01');
 INSERT INTO Price_History VALUES('Royal Sporting House', 'Ultraboost Shoes', 239, '2018-05-01', '2018-06-01');
@@ -109,3 +88,59 @@ INSERT INTO Price_History VALUES('Adidas', 'Pureboost RBL Shoes', 155, '2018-05-
 INSERT INTO Price_History VALUES('Adidas', 'Pureboost RBL Shoes', 145, '2018-06-01', '2018-08-01');
 INSERT INTO Price_History VALUES('Adidas', 'Pureboost RBL Shoes', 165, '2018-08-01', '2018-09-01');
 INSERT INTO Price_History VALUES('Adidas', 'Pureboost RBL Shoes', 150, '2018-09-01', '2018-11-01');
+
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 999, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 949, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 919, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 939, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 899, '2018-08-01', '2018-09-01');
+INSERT INTO Price_History VALUES('Family House', 'Quickdrive Washing', 799, '2018-09-01', '2018-11-01');
+
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 1199,'2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 1100,'2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 1129,'2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 1111,'2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 999,'2018-08-01', '2018-09-01');
+INSERT INTO Price_History VALUES('Family House', 'Ecobubble Washing', 1099,'2018-09-01', '2018-11-01');
+
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1699, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1599, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1455, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1529, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1329, '2018-08-01', '2018-09-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone X', 1600, '2018-09-01', '2018-11-01');
+
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1599, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1589, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1500, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1359, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1590, '2018-08-01', '2018-08-31');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XS', 1400, '2018-08-31', '2018-11-26');
+
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1759, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1729, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1719, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1699, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1688, '2018-08-01', '2018-08-31');
+INSERT INTO Price_History VALUES('Phone Buy', 'iPhone XR', 1700, '2018-08-31', '2018-11-26');
+
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1399, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1400, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1322, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1299, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1350, '2018-08-01', '2018-09-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone X', 1360, '2018-09-01', '2018-11-01');
+
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1699, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1600, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1629, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1528, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1600, '2018-08-01', '2018-08-31');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XS', 1679, '2018-08-31', '2018-11-26');
+
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1759, '2018-01-01', '2018-03-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1728, '2018-03-01', '2018-05-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1700, '2018-05-01', '2018-06-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1600, '2018-06-01', '2018-08-01');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1650, '2018-08-01', '2018-08-31');
+INSERT INTO Price_History VALUES('iChallenger', 'iPhone XR', 1800, '2018-08-31', '2018-11-26');
