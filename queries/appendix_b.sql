f16a7d30829bf1843ea3d65d9036e750bf27659d
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2018-10-30 16:35:43 +0800

    Add appendix b queries

---
+++ queries/appendix_b.sql
@@ -0,0 +1,47 @@
+-- Question 1
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
+
+-- Question 2
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
+
+-- Question 3
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
+
+-- Question 4
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
+
+
+-- Question 5
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
+
+SELECT 	product_name AS "Product", AVG(price) AS 'Avg Price'
+FROM 	Price_History
+WHERE 	product_name = 'iPhone XS' AND
+start_date >= '2018-08-01' AND
+end_date <= '2018-08-31'
+GROUP BY 	product_name;
